d68121432579ac52c49553c804109f2f3d80782d
JusticarExcellion <zacharyb6102@gmail.com>
2025-06-19 14:32:34 -0400

    Added Zooming

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -6,19 +6,34 @@ CameraMovement : MonoBehaviour
 {
     [SerializeField]
     private CinemachineCamera MainCamera;
+    [Header("Camera Movement Properties:")]
     [SerializeField]
     private float CameraMoveSpeed;
+    [SerializeField]
+    private float CameraZoomSpeed;
+    [SerializeField]
+    private float MaxHeight;
+    [SerializeField]
+    private float MinHeight;
 
     private void
     Update()
     {
+        Vector2 ScrollDelta = Input.mouseScrollDelta;
+        Vector3 CameraVector = new Vector3(0,ScrollDelta.y,0);
+        Vector3 CurrentPosition = MainCamera.transform.position;
+        CameraVector *= CameraZoomSpeed;
+        CurrentPosition += CameraVector;
+        CurrentPosition.y = Mathf.Clamp( CurrentPosition.y, MinHeight, MaxHeight );
+        MainCamera.transform.position = CurrentPosition;
+
         if( Input.GetMouseButton( 2 ) )
         {
             Vector3 MouseDelta = Input.mousePositionDelta;
-            Vector3 CameraVector = new Vector3( MouseDelta.x, 0, MouseDelta.y );
+            CameraVector = new Vector3( MouseDelta.x, 0 ,MouseDelta.y );
             CameraVector *= CameraMoveSpeed;
-
-            MainCamera.transform.position += CameraVector;
+            CurrentPosition += CameraVector;
+            MainCamera.transform.position = CurrentPosition;
         }
     }
 }
